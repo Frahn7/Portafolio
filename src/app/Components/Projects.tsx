124d2026ee5ca7595e0e1a2359139b5f0bc646c0
Francisco Villella <franciscovillella2003@gmail.com>
2024-03-31 17:29:07 -0300

    Agregando proyectos en donde trabaje

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -9,6 +9,13 @@ export const Projects = () => {
         </h1>
       </div>
       <div className=" flex flex-wrap justify-center gap-4 pb-10 ">
+        <a href="https://www.ccomplejoartmedia.com.ar/" target="_blank">
+          <Card
+            title="Complejo Artmedia"
+            description="Me dediqué a agregar características y solucionar problemas durante mi tiempo como desarrollador en DEPC."
+            tec=" React & Tailwind "
+          />
+        </a>
         <a
           href="https://lista-de-tareas-react-five.vercel.app/"
           target="_blank"
@@ -19,6 +26,13 @@ export const Projects = () => {
             tec=" React & Tailwind "
           />
         </a>
+        <a href="https://www.tododescuentos.com.ar/" target="_blank">
+          <Card
+            title="Todo descuentos"
+            description="Me dediqué a agregar características y solucionar problemas durante mi tiempo como desarrollador en DEPC."
+            tec=" React & Tailwind & TypeScript "
+          />
+        </a>
         <a href="https://rick-y-morty-fetch-api.vercel.app/" target="_blank">
           <Card
             title="Fetch Api"
@@ -26,17 +40,24 @@ export const Projects = () => {
             tec=" React & Bootstrap "
           />
         </a>
-        <a href="https://tic-tac-toe-theta-fawn-92.vercel.app/" target="_blank">
-          <Card
-            title="Tic-Tac-Toe"
-            description="Desarrollé el famoso juego de 3 en raya"
-            tec=" React & Tailwind "
-          />
-        </a>
         <a href="https://app-clima-lake.vercel.app/" target="_blank">
           <Card
             title="App del clima"
             description="Creé una aplicación sencilla de pronóstico del clima que se conecta y consume la API de OpenWeather"
+            tec=" React & Tailwind "
+          />
+        </a>
+        <a href="https://www.anba.org.ar/" target="_blank">
+          <Card
+            title="ANBA"
+            description="Me dediqué a agregar características y solucionar problemas durante mi tiempo como desarrollador en DEPC."
+            tec=" React & CSS "
+          />
+        </a>
+        <a href="https://tic-tac-toe-theta-fawn-92.vercel.app/" target="_blank">
+          <Card
+            title="Tic-Tac-Toe"
+            description="Desarrollé el famoso juego de 3 en raya"
             tec=" React & Tailwind "
           />
         </a>
