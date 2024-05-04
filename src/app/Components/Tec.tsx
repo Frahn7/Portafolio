cd66c0d5737c0be55bbff0a9ba96f60c720a401f
Francisco Villella <franciscovillella2003@gmail.com>
2024-05-04 13:04:09 -0300

    add icons and efects

--- src/app/Components/Tec.tsx
+++ src/app/Components/Tec.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoCss3 } from "react-icons/io";
+import { RiJavascriptFill } from "react-icons/ri";
+import { FaReact } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNodeJs } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
+import { FaLaravel } from "react-icons/fa";
+import { FaAws } from "react-icons/fa";
+import { SiMysql } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { BsFillBootstrapFill } from "react-icons/bs";
+
 interface TecProps {
-  title: string;
   className: string;
+  icon: React.ReactNode;
 }
 
-function TecnologiasCard({ title, className }: TecProps) {
+function TecnologiasCard({ className, icon }: TecProps) {
   const textColor =
-    "bg-badge-background h-[60px] w-[250px] content-center items-center rounded-md bg-black pt-4 text-center align-middle text-[17px] font-medium " +
+    "bg-badge-background h-[90px] w-[200px] flex  content-center items-center rounded-md justify-center bg-black text-center align-middle font-medium text-[60px] " +
     className;
 
-  return <div className={textColor}>{title}</div>;
+  return <div className={textColor}>{icon}</div>;
 }
 
 export const Tec = () => {
@@ -20,18 +35,54 @@ export const Tec = () => {
         </h1>
       </div>
       <div className="mt-4 flex flex-wrap justify-center gap-2 px-[25px] text-center align-middle">
-        <TecnologiasCard title="HTML" className="text-orange-600" />
-        <TecnologiasCard title="CSS" className="text-blue-500" />
-        <TecnologiasCard title="JAVASCRIPT" className="text-yellow-500" />
-        <TecnologiasCard title="REACT" className="text-blue-700" />
-        <TecnologiasCard title="NEXT JS" className="text-white" />
-        <TecnologiasCard title="NODEJS" className="text-green-600" />
-        <TecnologiasCard title="PHP" className="text-violet-400" />
-        <TecnologiasCard title="LARAVEL" className="text-red-500" />
-        <TecnologiasCard title="AWS" className="text-yellow-600" />
-        <TecnologiasCard title="MYSQL" className="text-white" />
-        <TecnologiasCard title="TAILWIND" className="text-blue-700" />
-        <TecnologiasCard title="BOOTSTRAP" className="text-violet-900" />
+        <TecnologiasCard
+          icon={<FaHtml5 />}
+          className="text-orange-600 hover:text-orange-400"
+        />
+        <TecnologiasCard
+          icon={<IoLogoCss3 />}
+          className="text-blue-500 hover:text-blue-300"
+        />
+        <TecnologiasCard
+          icon={<RiJavascriptFill />}
+          className="text-yellow-500 hover:text-yellow-300"
+        />
+        <TecnologiasCard
+          icon={<FaReact />}
+          className="text-blue-700 hover:text-blue-500"
+        />
+        <TecnologiasCard
+          icon={<TbBrandNextjs />}
+          className="text-white hover:text-gray-300"
+        />
+        <TecnologiasCard
+          icon={<FaNodeJs />}
+          className="text-green-600 hover:text-green-400"
+        />
+        <TecnologiasCard
+          icon={<FaPhp />}
+          className="text-violet-400 hover:text-violet-200"
+        />
+        <TecnologiasCard
+          icon={<FaLaravel />}
+          className="text-red-500 hover:text-red-300"
+        />
+        <TecnologiasCard
+          icon={<FaAws />}
+          className="text-yellow-600 hover:text-yellow-400"
+        />
+        <TecnologiasCard
+          icon={<SiMysql />}
+          className="text-white hover:text-gray-300"
+        />
+        <TecnologiasCard
+          icon={<SiTailwindcss />}
+          className="text-blue-700 hover:text-blue-500"
+        />
+        <TecnologiasCard
+          icon={<BsFillBootstrapFill />}
+          className="text-violet-900 hover:text-violet-700"
+        />
       </div>
     </div>
   );
